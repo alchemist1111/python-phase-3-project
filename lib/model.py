e03eeb5b1ee5e242b616c0406780184b39f7910d
alchemist1111 <emoyoeugine@gmail.com>
2024-06-13 11:40:57 +0300

    added solution

--- lib/model.py
+++ lib/model.py
@@ -5,3 +5,10 @@ from sqlalchemy.orm import relationship
 
 # Define the base class for the ORM models
 Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    author = relationship('Author', backref='books')
