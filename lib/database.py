10637f5fcd687864e2f282420f53188307dea550
alchemist1111 <emoyoeugine@gmail.com>
2024-06-13 12:14:53 +0300

    added solution

--- lib/database.py
+++ lib/database.py
@@ -1,9 +1,10 @@
 # Database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import config
 
 # Setup the database engine
-engine = create_engine('sqlite:///library.db')
+engine = create_engine(config.DATABASE_URI)
 
 # Setup the session
 Session = sessionmaker(bind=engine)
