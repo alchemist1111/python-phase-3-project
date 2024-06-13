e3fbffa59ffdb65df13ba3323ddae425ffee44eb
alchemist1111 <emoyoeugine@gmail.com>
2024-06-13 11:54:26 +0300

    added function to get all books

--- lib/cli.py
+++ lib/cli.py
@@ -1,3 +1,9 @@
 import argparse
 from Database import session  # Import the session created in Database.py
 from model import Book, Author, Borrower, BorrowedBook  # Import the models
+
+# Function to get all books
+def get_books():
+    result = session.query(Book).all()
+    for book in result:
+        print(f"Title: {book.title}, Author: {book.author.name}")
